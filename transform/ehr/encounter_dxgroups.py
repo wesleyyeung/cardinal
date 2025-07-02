9c03bd2f56f87d9c6106061a652c6d5fffc2a17a
Wesley Yeung <wesleyyeung123@gmail.com>
2025-07-02 14:04:09 +0800

    fix /transform/ehr/encounter_dxgroup.py logic

--- transform/ehr/encounter_dxgroups.py
+++ transform/ehr/encounter_dxgroups.py
@@ -24,20 +24,22 @@ class EHREncounterDxGroups(BaseTransform):
                 combined_codes[db][dx] = codes.get(dx)
             dx_keys += list(codes.keys())
         
-        df['combined_diagnosis'] = df['primary_diagnosis'].astype(str).str.replace('.0','') + ',' + df['secondary_diagnosis'].astype(str).str.replace('.0','')
+        df.loc[df['db_source']=='edw','combined_diagnosis'] = df.loc[df['db_source']=='edw','primary_diagnosis'].astype(str).str.replace('.0','') + ',' + df.loc[df['db_source']=='edw','secondary_diagnosis'].astype(str).str.replace('.0','')
+        df.loc[df['db_source']=='dts','combined_diagnosis'] = df.loc[df['db_source']=='dts','primary_diagnosis'].astype(str) + ',' + df.loc[df['db_source']=='dts','secondary_diagnosis'].astype(str)
+        df.loc[:,'combined_diagnosis'] = df['combined_diagnosis'].astype(str).str.replace(',nan','').str.replace('nan,','').str.replace('nan','')
 
         dx_keys = list(set(dx_keys))
         df[dx_keys] = 0
-        
+
         for db in db_keys:
             for dx in combined_codes[db].keys():
                 code_list = combined_codes[db][dx] 
                 input = df.loc[df['db_source']==db,'combined_diagnosis']
                 if len(input) == 0:
                     continue
-                df.loc[df['db_source']==db,dx] = input.apply(lambda row: has_any_code(row,code_list))    
-        df = df.drop(columns=['primary_diagnosis','secondary_diagnosis','combined_diagnosis'])
-
+                output = input.apply(lambda row: has_any_code(row,code_list)) 
+                df.loc[df['db_source']==db,dx] = output
+   
         df['death'] = df['visit_outcome'].isin(['Death Coroners','Death Non-Coroners','Deceased']).astype(int)
-        cols = ['visit_id','mrn_sha1','visit_date'] + list(combined_codes[db].keys()) + ['death']
+        cols = ['visit_id','mrn_sha1','visit_date','location','sub_location','combined_diagnosis'] + list(combined_codes[db].keys()) + ['death']
         return df[cols], "dxgroups"
