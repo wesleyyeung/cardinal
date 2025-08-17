d17add186249723433fbe9fea3911ed5c9b682f2
Wesley Yeung <wesleyyeung123@gmail.com>
2025-08-17 19:49:24 +0800

    edit cleaning/ccd/ohca_cleaner.py to enforce datetime format

--- cleaning/ccd/ohca_cleaner.py
+++ cleaning/ccd/ohca_cleaner.py
@@ -6,8 +6,8 @@ class CCDOHCACleaner(BaseCleaner):
         super().__init__()
 
     def custom_clean(self, df: pd.DataFrame) -> pd.DataFrame:
-        df['arrival_dt'] = df['Date of arrival at ED'] + ' ' + df['Time of arrival at ED']
-        df['arrival_dt'] = pd.to_datetime(df['arrival_dt'],format="%d/%m/%Y %H:%M:%S",errors='coerce')
+        df['time_of_arrival_at_ed'] = df['time_of_arrival_at_ed'].apply(lambda row: row if sum([1 if char == ':' else 0 for char in row]) == 2 else row +':00')
+        df['arrival_dt'] = pd.to_datetime(df['date_of_arrival_at_ed'] + ' ' + df['time_of_arrival_at_ed'],format='%d/%m/%Y %H:%M:%S',errors='raise')
         df['Date of Discharge or Death'] = pd.to_datetime(df['Date of Discharge or Death'],format="%d/%m/%Y", errors='coerce')
         return df.rename(columns={
             'mrn_sha1':'subject_id'
